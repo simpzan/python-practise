1e547f1f35127e1f04a18224ed08acb84c36a341
Samuel Zhang <simpzan@gmail.com>
2023-08-03 21:44:17 +0800

    add initial implementation from her

--- dictionary/dict.py
+++ dictionary/dict.py
@@ -1,13 +1,29 @@
 import sys
 
 def lookup(word):
-    return f"the fake definition of {word}"
+    s='0'
+    z=open('dict.txt')
+    for a in z:
+        a=a.strip()
+        if a.startswith('[' + word + ' |'):
+            s=a
+            break
+    if s=='0':
+        return "aabb"
+    else:
+         q=2
+         for w in s:
+             if w=="|":
+                 break
+             else: q+=1
+         return s[q:-1]
 
 def main():
     argv = sys.argv
     # print(argv)
     word = argv[1] if len(argv) >= 2 else "thinker"
     definition = lookup(word)
+    definition = definition.replace('\\n', '\n')
     print(f"The definition of '{word}':\n{definition}")
 
 main()
