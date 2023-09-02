2a0029e2e80d3d8895eabc8758276cccca690c58
Sally0252 <142795713+Sally0252@users.noreply.github.com>
2023-09-02 21:19:35 +0800

    remove lookup() (#1)
    * remove lookup()
    
    * rename lookup2() to lookup()

--- dictionary/dict.py
+++ dictionary/dict.py
@@ -1,6 +1,6 @@
 import sys
 
-def lookup2(word):
+def lookup(word):
     file = open('dict.txt')
     prefix = f'[{word} || '
     result = None
@@ -11,24 +11,6 @@ def lookup2(word):
     file.close()
     if result is None: return ''
     return result[len(prefix):-2]
-
-def lookup(word):
-    s='0'
-    z=open('dict.txt')
-    for a in z:
-        a=a.strip()
-        if a.startswith('[' + word + ' |'):
-            s=a
-            break
-    if s=='0':
-        return "aabb"
-    else:
-         q=2
-         for w in s:
-             if w=="|":
-                 break
-             else: q+=1
-         return s[q:-1]
 
 def writing(word):
     x=0
@@ -46,7 +28,7 @@ def main():
     argv = sys.argv
     # print(argv)
     word = argv[1] if len(argv) >= 2 else "thinker"
-    definition = lookup2(word)
+    definition = lookup(word)
     definition = definition.replace('\\n', '\n')
     print(f"The definition of '{word}':\n{definition}")
     writing(word)
